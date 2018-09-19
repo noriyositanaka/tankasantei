0783cd2156316e195ae5572e0590c35a00f12927
noriyositanaka <43325084+noriyositanaka@users.noreply.github.com>
2018-09-19 22:07:30 +0900

    原紙価格の永続的補完(Done)

--- app/src/main/java/com/example/noriyoshi/Tankasantei/GensiKakakuSettei.java
+++ app/src/main/java/com/example/noriyoshi/Tankasantei/GensiKakakuSettei.java
@@ -4,19 +4,40 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.os.Bundle;
-import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
 public class GensiKakakuSettei extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
 
+         /*
+        保存済み原紙価格の取得
+         */
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        //SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        String lnr = sharedPreferences.getString("LINER_KAKAKU","0");
+        Log.d("LinerKiro", "LinerKiro is "+lnr);
+        String hsn = sharedPreferences.getString("HUTUU_KAKAKU","0");
+        Log.d("SinKiro", "SinKiro is "+hsn);
+        String ksn = sharedPreferences.getString("KYOUKA_KAKAKU","0");
+        Log.d("KyoukaKiro", "KyoukaKiro is "+ksn);
+
+        TextView tv;
+        /*
+        tv = findViewById(R.id.linerkakaku);
+        tv.setText(lnr);
+        tv=findViewById(R.id.hutuusinkakaku);
+        tv.setText(hsn);
+        tv=findViewById(R.id.kyoukasinkakaku);
+        tv.setText(ksn);
+        */
     /*
 m戻るボタン押し下げ
  */
@@ -36,6 +57,7 @@ m戻るボタン押し下げ
  ライナーキロ価格取得
   */
         TextView ln = findViewById(R.id.linerkakaku);
+        ln.setText(lnr);
         ln.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
@@ -47,8 +69,9 @@ m戻るボタン押し下げ
                     SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                     SharedPreferences.Editor editor=sharedPreferences.edit();
                     editor.putString("LINER_KAKAKU",s);
-                    editor.apply();
 
+                    editor.apply();
+     //               Log.d("LinerKiro", "LinerKiro is "+s);
                     double d = (double) Integer.parseInt(s);
                     NumBase.setLinerkiro(d);
                 }
@@ -58,6 +81,7 @@ m戻るボタン押し下げ
 普通芯キロ価格取得
   */
         TextView fs = findViewById(R.id.hutuusinkakaku);
+        fs.setText(hsn);
         fs.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
@@ -69,6 +93,7 @@ m戻るボタン押し下げ
                     SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                     SharedPreferences.Editor editor=sharedPreferences.edit();
                     editor.putString("HUTUU_KAKAKU",s);
+ //                   Log.d("LinerHutuusin", "HutuuSinKiro is "+s);
                     editor.apply();
 
                     double d = (double) Integer.parseInt(s);
@@ -80,8 +105,10 @@ m戻るボタン押し下げ
 強化芯キロ価格取得
   */
         TextView ks = findViewById(R.id.kyoukasinkakaku);
+        ks.setText(ksn);
         ks.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
+
             public void onFocusChange(View v, boolean hasFocus) {
                 if (!hasFocus) {
                     TextView tu;
@@ -91,6 +118,7 @@ m戻るボタン押し下げ
                     SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                     SharedPreferences.Editor editor=sharedPreferences.edit();
                     editor.putString("KYOUKA_KAKAKU",s);
+   //                 Log.d("KyoukaKiro", "KyoukaKiro is "+s);
                     editor.apply();
 
                     double d = (double) Integer.parseInt(s);
