eec9a628a94f4ca4bca65c7152ce850e72af814b
noriyositanaka <43325084+noriyositanaka@users.noreply.github.com>
2018-09-22 21:07:00 +0900

    原紙価格の永続的補完(Done)

--- app/src/main/java/com/example/noriyoshi/Tankasantei/GensiKakakuSettei.java
+++ app/src/main/java/com/example/noriyoshi/Tankasantei/GensiKakakuSettei.java
@@ -20,14 +20,13 @@ public class GensiKakakuSettei extends AppCompatActivity {
         保存済み原紙価格の取得
          */
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        //SharedPreferences.Editor editor = sharedPreferences.edit();
 
         String lnr = sharedPreferences.getString("LINER_KAKAKU", "0");
-        Log.d("LinerKiro", "LinerKiro is " + lnr);
+//        Log.d("LinerKiro", "LinerKiro is " + lnr);
         String hsn = sharedPreferences.getString("HUTUU_KAKAKU", "0");
-        Log.d("SinKiro", "SinKiro is " + hsn);
+//        Log.d("SinKiro", "SinKiro is " + hsn);
         String ksn = sharedPreferences.getString("KYOUKA_KAKAKU", "0");
-        Log.d("KyoukaKiro", "KyoukaKiro is " + ksn);
+//        Log.d("KyoukaKiro", "KyoukaKiro is " + ksn);
 
         TextView ln = findViewById(R.id.linerkakaku);
         ln.setText(lnr);
@@ -42,103 +41,47 @@ public class GensiKakakuSettei extends AppCompatActivity {
         super.onResume();
 
 
-    /*
-m戻るボタン押し下げ
- */
+/*
+戻るボタン押し下げ
+*/
         View modoru = findViewById(R.id.sub_modoru);
         modoru.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-/*
-ライナー価格取得と保管
- */
 
 /*
-普通芯価格取得と保管
+ライナー価格取得
  */
+                TextView ts=findViewById(R.id.linerkakaku);
+                String s = ts.getText().toString();
 
 /*
-強化芯価格取得と保管
+普通芯価格取得
+ */
+                TextView tv =findViewById(R.id.hutuusinkakaku);
+                String t = tv.getText().toString();
+
+/*
+強化芯価格取得
 
  */
+                TextView tu =findViewById(R.id.kyoukasinkakaku);
+                String u = tu.getText().toString();
+/*
+保管
+ */
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                SharedPreferences.Editor editor=sharedPreferences.edit();
+                editor.putString("LINER_KAKAKU",s);
+                editor.putString("HUTUU_KAKAKU",t);
+                editor.putString("KYOUKA_KAKAKU",u);
+                editor.apply();
 
-
-
-
-
+                NumBase.setLinerkiro(s);
+                NumBase.setSinkiro(t);
+                NumBase.setKyoukakiro(u);
 
                 finish();
-            }
-        });
-
- /*
- ライナーキロ価格取得
-  */
-        TextView ln = findViewById(R.id.linerkakaku);
-        ln.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    TextView ts;
-                    ts = (TextView) v;
-                    String s = ts.getText().toString();
-
-                    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    SharedPreferences.Editor editor=sharedPreferences.edit();
-                    editor.putString("LINER_KAKAKU",s);
-
-                    editor.apply();
-     //               Log.d("LinerKiro", "LinerKiro is "+s);
-                    double d = (double) Integer.parseInt(s);
-                    NumBase.setLinerkiro(d);
-                }
-            }
-        });
- /*
-普通芯キロ価格取得
-  */
-        TextView fs = findViewById(R.id.hutuusinkakaku);
-        fs.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    TextView tv;
-                    tv = (TextView) v;
-                    String s = tv.getText().toString();
-
-                    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    SharedPreferences.Editor editor=sharedPreferences.edit();
-                    editor.putString("HUTUU_KAKAKU",s);
- //                   Log.d("LinerHutuusin", "HutuuSinKiro is "+s);
-                    editor.apply();
-
-                    double d = (double) Integer.parseInt(s);
-                    NumBase.setSinkiro(d);
-                }
-            }
-        });
- /*
-強化芯キロ価格取得
-  */
-        TextView ks = findViewById(R.id.kyoukasinkakaku);
-        ks.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    TextView tu;
-                    tu = (TextView) v;
-                    String s = tu.getText().toString();
-
-                    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                    SharedPreferences.Editor editor=sharedPreferences.edit();
-                    editor.putString("KYOUKA_KAKAKU",s);
-   //                 Log.d("KyoukaKiro", "KyoukaKiro is "+s);
-                    editor.apply();
-
-                    double d = (double) Integer.parseInt(s);
-                    NumBase.setKyoukakiro(d);
-                }
             }
         });
     }
